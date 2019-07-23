2f168f12b56e1b4868d8ccaba7723ec975d31599
josephwele <josiwele@gmail.com>
2019-07-23 14:58:02 -0700

    index.js router add

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,58 @@
-// import from models/user.js (mongoose query builder)
-const User = require('../models/user.js')
+const express = require('express')
+const passport = require('passport')
+const Account = require('../models/account')
+const router = express.Router()
 
-module.exports = function (app) {
-  // sign up route
-  app.post('/signup', (req, res) => {
-    const firstName = req.body.firstName
-    const newUser = new User({ firstName })
+router.get('/', (req, res) => {
+    res.render('index', { user: req.user })
+})
 
-    newUser.save()
-      .then(() => res.json('User added'))
-      .catch(err => res.status(400).json('Error: ' + err))
-  })
-}
+router.get('/register', (req, res) => {
+    res.render('register', {})
+})
+
+router.post('/register', (req, res, next) => {
+    Account.register(new Account({ username: req.body.username }), req.body.password, (err, account) => {
+        if (err) {
+            return res.render('register', { error: err.message })
+        }
+
+        passport.authenticate('local')(req, res, () => {
+            req.session.save((err) => {
+                if (err) {
+                    return next(err)
+                }
+                res.redirect('/')
+            })
+        })
+    })
+})
+
+router.get('/login', (req, res) => {
+    res.render('login', { user: req.user, error: req.flash('error') })
+})
+
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), (req, res, next) => {
+    req.session.save((err) => {
+        if (err) {
+            return next(err)
+        }
+        res.redirect('/')
+    })
+})
+
+router.get('/logout', (req, res, next) => {
+    req.logout()
+    req.session.save((err) => {
+        if (err) {
+            return next(err)
+        }
+        res.redirect('/')
+    })
+})
+
+router.get('/form', (req, res) => {
+    res.status(200).send('pong!')
+})
+
+module.exports = router
