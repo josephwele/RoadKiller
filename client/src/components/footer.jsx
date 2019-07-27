dee54c708e0ad87b1b3d78a8bccd6fe3dea5d4c2
solomon <solomonghebrelibanos@gmail.com>
2019-07-27 12:25:49 -0700

    added some fuetures of rect 3

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from  'react';
+
+
+
+export class Footer extends React.Component{
+    render(){
+
+        return(
+<div class="card-footer text-muted" style={{position: 'absolute', left:'0px', bottom: '0px', width:'100%', textAlign:'center', padding: '20px', backgroundColor:'#B2B3AE'}}>
+   <h3>RoadKiller</h3>
+  </div>
+        )
+    }
+}
