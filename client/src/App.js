baafeb333510eed432449aee3c2ee6c104a3c90f
Jayant Rai <jayant.chamling@outlook.com>
2019-07-31 22:46:13 -0700

    tried running

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { NavBar } from './components/navBar.jsx'
 import { SignUp } from './components/signUp.jsx'
 import { Footer } from './components/footer.jsx'
+// import { ImageSlider } from './src/data/imageslider.js'
 
 function App () {
   return (
     <div className='App'>
       <NavBar />
+        {/* <ImageSlider />
+      </NavBar> */}
       <SignUp />
       <Footer />
 
