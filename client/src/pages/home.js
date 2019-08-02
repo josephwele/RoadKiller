2986e432d8b45eaf69a471d55ab7ce10dca4456c
josephwele <josiwele@gmail.com>
2019-08-01 20:12:07 -0700

    file arrengment done

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { NavBar } from '../components/navBar'
-import { SignUp } from '../components/signUp'
-import { Footer } from './../components/footer'
+import { NavBar } from '../components/navBar/navBar'
+import { SignUp } from '../components/signUp/signUp'
+import { Footer } from './../components/footer/footer'
 import API from '../utils/API';
 import Jumbotron from '../components/jumbotron/jumbotron';
 
